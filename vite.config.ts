e3cb5dab07d34b766da3f8cb4c371e2d303e24be
Giiver <jeanfrancoisvilm@gmail.com>
2025-08-18 20:29:54 +0200

    Use relative paths for PWA start_url and scope

--- vite.config.ts
+++ vite.config.ts
@@ -33,8 +33,8 @@ export default defineConfig({
         background_color: '#ffffff',
         display: 'fullscreen',
         orientation: 'landscape-primary',
-        start_url: '/T-Ponge/',
-        scope: '/T-Ponge/',
+        start_url: './',
+        scope: './',
         icons: [
           {
             src: 'pwa-192x192.png',
